af794937074079716f8a28bd0fc49b2562cbde0a
Sukeedori <kpee142857@gmail.com>
2024-04-07 22:27:28 -0500

    Hangman version 1.0

---
+++ scripts/word-list.js
@@ -0,0 +1,21 @@
+const wordList = [
+    {
+        word: "arbol",
+        hint: "Planta de tallo leñoso y elevado, que se ramifica a cierta altura del suelo."
+    },
+
+    {
+        word: "avion",
+        hint: " Vehículo, provisto de alas y uno o varios motores, capaz de desplazarse por el aire."
+    },
+
+    {
+        word: "javascript",
+        hint: "Lenguaje de programación, utilizado mayoritariamente para el desarrollo web."
+    },
+
+    {
+        word: "ñaño",
+        hint: "Otra manera de llamar a un hermano."
+    },
+];

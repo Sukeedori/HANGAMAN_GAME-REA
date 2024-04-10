f151491af0bd88689e6cbd455d14c3ccdad60161
Sukeedori <kpee142857@gmail.com>
2024-04-10 09:51:21 -0500

    Hangman version 1.3

--- scripts/word-list.js
+++ scripts/word-list.js
@@ -1,21 +1,71 @@
 const wordList = [
     {
-        word: "arbol",
-        hint: "Planta de tallo leñoso y elevado, que se ramifica a cierta altura del suelo."
+        word: "web",
+        hint: "Sistema de gestión de información más popular para la trasmisión de datos a través de internet."
     },
 
     {
-        word: "avion",
-        hint: " Vehículo, provisto de alas y uno o varios motores, capaz de desplazarse por el aire."
+        word: "http",
+        hint: "Protocolo de transferencia de hipertexto, protocolo subyacente utilizado por la World Wide Web."
     },
 
     {
         word: "javascript",
-        hint: "Lenguaje de programación, utilizado mayoritariamente para el desarrollo web."
+        hint: "Lenguaje de programación, utilizado para crear páginas web interactivas."
     },
 
     {
-        word: "ñaño",
-        hint: "Otra manera de llamar a un hermano."
+        word: "html",
+        hint: "Lenguaje de marcado de hipertextos, que nos permite definir la estructura de una página web. "
+    },
+
+    {
+        word: "css",
+        hint: "Hojas de estilo en cascada, utilizado para el estilado de una páginas web."
+    },
+
+    {
+        word: "programacion",
+        hint: "Acción de crear aplicaciones a través del desarrollo de un código fuente, que se basa en el conjunto de instrucciones."
+    }, 
+
+    {
+        word: "software",
+        hint: "Parte interna de un computador."
+    },
+
+    {
+        word: "hadware",
+        hint: "Parte externa de un computador."
+    },
+
+    {
+        word: "linux",
+        hint: "Sistema operativo creado por Linus Torvalds, conocido por su código abierto, su robustez y su flexibilidad, "
+    },
+
+    {
+        word: "algoritmo",
+        hint: "Es preciso, definido y finito."
+    },
+
+    {
+        word: "variable",
+        hint: "Elemento de datos con nombre cuyo valor puede cambiar durante el curso de la ejecución de un programa."
+    }, 
+
+    {
+        word: "frontend",
+        hint: "Parte del desarrollo web que se dedica a la parte frontal de un sitio web."
+    },
+
+    {
+        word: "backend",
+        hint: "Parte o rama del desarrollo web encargada de que toda la lógica de una página funcione."
+    }, 
+
+    {
+        word: "dominio",
+        hint: "Nombre que sirve para identificar a un sitio web."
     },
 ];
